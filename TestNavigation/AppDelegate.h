099897ac9c27dc7a6a869a9b02520d9b31c5e85e
Daniel Hahn <Handsaw@Daniels-Mac.local>
2013-08-11 15:16:17 -0700

    Initial Commit

---
+++ TestNavigation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestNavigation
+//
+//  Created by Daniel Hahn on 8/11/13.
+//  Copyright (c) 2013 StarkWeather. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestNavigation/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  TestNavigation
+//
+//  Created by Daniel Hahn on 8/11/13.
+//  Copyright (c) 2013 StarkWeather. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
